645498e1211213b9b975ad074aa4107276b3c48e
mahaliyanur <mahaliyanurbimakusuma@gmail.com>
2021-01-19 22:41:23 +0700

    login

--- koneksiDB.php
+++ koneksiDB.php
@@ -16,12 +16,12 @@ if (!$conn) {
 	echo "connection failed";
 }
 
-$sql = "SELECT * FROM account WHERE username = '$email' AND password = '$password' LIMIT 1 ";
+$sql = "SELECT * FROM account WHERE email = '$email' AND password = '$password' LIMIT 1 ";
 $result = mysqli_query($conn,$sql);
 if (!empty($result)) {	
 	if (mysqli_num_rows($result) == 1) {
 		session_start();
-		$_SESSION['username'] = $username;
+		$_SESSION['email'] = $email;
 		header("location: indexlogged.php");
 		exit();
 	}
