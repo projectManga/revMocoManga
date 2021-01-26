d3aa4ece5600950ff198bf137900402c593f21da
mahaliyanur <mahaliyanurbimakusuma@gmail.com>
2021-01-26 09:49:55 +0700

    clear

--- log_bottom.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-if(isset($_SESSION['email'])): ?>
-    <li><a href="./pslogot.php">Logout</a></li>
-<?php else: ?>
-    <li><a href="./login.php">Login</a></li>
-<?php endif; ?>
